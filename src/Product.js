e6618b1fabc0ce2e0f30c85c050a35352d4a12f9
White Bear <izdebski.krzysztof@gmail.com>
2018-03-01 18:34:07 +0100

    first react app

---
+++ src/Product.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    constructor(){
+        super();
+        //this.buttonClicked = this.buttonClicked.bind(this);
+    }
+    state = {
+        showDetails: false
+    }
+    buttonClicked(){
+        this.setState((prev) => {
+            console.log('prev',prev);
+            return {showDetails:!prev.showDetails}}
+        );
+    }
+    render() {
+        return (
+            <div>
+                <span> {this.props.product.name} </span>
+                <img src={this.props.product.img}/> 
+                <button onClick={this.buttonClicked} >expand </button>
+                {this.state.showDetails ? <span >{this.props.product.desc} </span> : <div></div> }
+            </div>
+        );
+    }
+}
+
+export default Product;
---
+++ src/ProductList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Product from './Product';
+
+class ProductList extends Component {
+
+    state = {
+        tasks: []
+    }
+    componentDidMount() {
+        fetch('data/products.json')
+            .then((promise) => promise.json())
+            .then((tasks) => {
+                console.log('fetch prods', tasks)
+                this.setState({ tasks: tasks });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <button >Reverse</button>
+                <h1 > My tasks: </h1>
+                {this.state.tasks.map(t => <Product key={t.name} product={t} ></Product>)}
+            </div>
+        );
+    }
+}
+
+export default ProductList;
